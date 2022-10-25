80d1f0fa595e0dc530ac24b89ef4b795ad57c024
MaiTalaat <mai.capmas@gmail.com>
2022-10-25 23:45:35 +0200

    First commit - Classes added

---
+++ src/SalesInvoiceGenerator/Models/InvoiceLine.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SalesInvoiceGenerator.Models;
+
+/**
+ *
+ * @author mtalaat
+ */
+public class InvoiceLine {
+    public String itemName;
+    public float itemPrice;
+    public int count;
+    
+    
+}
